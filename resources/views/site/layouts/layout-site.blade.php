68b9d582ac2ddcf7acea297085dbb40f8b7146f4
Yve Jose Zulatto <38947675+zulatto@users.noreply.github.com>
2021-11-15 21:46:36 -0300

    add new layout site

---
+++ resources/views/site/layouts/layout-site.blade.php
@@ -0,0 +1,247 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <link rel="stylesheet" href="src/css/index.css">
+    <title>Profind</title>
+</head>
+<body>
+    <div class="header">
+        <div id="logo">
+            <h1>Profind</h1>
+        </div>
+
+        <div class="menu aux">
+            <ul>
+                <li><a href="#" class="menu_aux">Entrar</a></li>
+                <li><a href="#" class="menu_aux">Como Funciona</a></li>
+            </ul>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li>Voce quer ser um </li>
+                <li><a href="#">Pro</a></li>
+                <li> ou um </li>
+                <li><a href="#">Find</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="section hero">
+        <div class="hero_left">
+            <div class="title_main">
+                Conectamos quem sabe <br>com quem precisa.
+            </div>
+            <div class="subtitle">
+                Se voce procura alguem procure conosco.
+            </div>
+            <input class="input_search" type="text" name="search" placeholder="Digite o que procura">
+        <input class="submit_search" type="submit" value="Buscar">
+        </div>
+        <div class="hero_right">
+            <img src="assets/images/martelo-de-unha.png" alt="martelo-de-unha" id="image-hero">
+        </div>
+        </div>
+        <div class="arrow">
+            <i class="uil uil-angle-down"></i>
+        </div>
+        <div class="title-menu_services">
+            Todas As Categorias
+        </div>
+    <div class="section">
+        <div class="title">Principais Serviços</div>
+        <div class="card-emphasis">
+            <div class="card">
+                <div class="image-card">
+                    <img src="assets/images/Marceneiro-Trabalhando.jpg" id="image-card" alt="Marceneiro-Trabalhando">
+                </div>
+                <div class="card-text">
+                    Marcenaria
+                </div>
+                <div class="card-text card-budget">
+                    Solicite um Orçamento   
+                </div>
+            </div>
+            <div class="card">
+                <div class="image-card">
+                    <img src="assets/images/Marceneiro-Trabalhando.jpg" id="image-card" alt="Marceneiro-Trabalhando">
+                </div>
+                <div class="card-text">
+                    Marcenaria
+                </div>
+                <div class="card-text card-budget">
+                    Solicite um Orçamento   
+                </div>
+            </div>
+            <div class="card">
+                <div class="image-card">
+                    <img src="assets/images/Marceneiro-Trabalhando.jpg" id="image-card" alt="Marceneiro-Trabalhando">
+                </div>
+                <div class="card-text">
+                    Marcenaria
+                </div>
+                <div class="card-text card-budget">
+                    Solicite um Orçamento   
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="section">
+        <div class="title">Alguns feedbacks para convencer voce</div>
+        <div class="feedbacks">
+            <div class="feedback-card">
+                <i class="uil uil-comment-alt-share"></i>
+                <span>Fui muito bem atendida, 
+                    o profissional foi muito educado 
+                    e fez um trabalho de qualidade. 
+                    Valeu a pena, orçamento grátis 
+                    e não é careiro. <br><br>
+    
+                    <strong>Serviço de Pedreiro</strong>,
+                    Ana Paula Contratou um 
+                    Pedreiro em São Paulo, SP</span>
+            </div>
+            <div class="feedback-card">
+                <i class="uil uil-comment-alt-share"></i>
+                <span>Fui muito bem atendida, 
+                    o profissional foi muito educado 
+                    e fez um trabalho de qualidade. 
+                    Valeu a pena, orçamento grátis 
+                    e não é careiro. <br><br>
+    
+                    <strong>Serviço de Pedreiro</strong>,
+                    Ana Paula Contratou um 
+                    Pedreiro em São Paulo, SP</span>
+            </div>
+            <div class="feedback-card">
+                <i class="uil uil-comment-alt-share"></i>
+                <span>Fui muito bem atendida, 
+                    o profissional foi muito educado 
+                    e fez um trabalho de qualidade. 
+                    Valeu a pena, orçamento grátis 
+                    e não é careiro. <br><br>
+    
+                    <strong>Serviço de Pedreiro</strong>,
+                    Ana Paula Contratou um 
+                    Pedreiro em São Paulo, SP</span>
+            </div>
+        </div>
+    </div>
+    <div class="section">
+        <div class="title">Como Funciona</div>
+        <div class="subtitle">
+            Profind é a maior plataforma de contratação de serviços do Brasil. 
+            Conectamos Profissionais de todo o Brasil com pessoas solicitando serviço, 
+            atendendo com qualidade, facilidade e rapidez todos os tipos de necessidade.
+        </div>
+        <div class="works">
+            <div class="step-find">
+                <div class="title-step">
+                    Faça o seu pedido
+                </div>
+                <div class="icon-step">
+                    <i class="uil uil-list-ul"></i>
+                </div>
+                <div class="text-step">
+                    Fale o que você precisa. É rápido e de graça!
+                </div> 
+            </div>
+            <div class="step-find">
+                <div class="title-step">
+                    Faça o seu pedido
+                </div>
+                <div class="icon-step">
+                    <i class="uil uil-list-ul"></i>
+                </div>
+                <div class="text-step">
+                    Fale o que você precisa. É rápido e de graça!
+                </div> 
+            </div>
+            <div class="step-find">
+                <div class="title-step">
+                    Faça o seu pedido
+                </div>
+                <div class="icon-step">
+                    <i class="uil uil-list-ul"></i>
+                </div>
+                <div class="text-step">
+                    Fale o que você precisa. É rápido e de graça!
+                </div> 
+            </div>
+        </div>
+    </div>
+    <div class="section">
+        <div class="title">Como Funciona</div>
+        <div class="subtitle">
+            Profind é a maior plataforma de contratação de serviços do Brasil. 
+            Conectamos Profissionais de todo o Brasil com pessoas solicitando serviço, 
+            atendendo com qualidade, facilidade e rapidez todos os tipos de necessidade.
+        </div>
+        <div class="works">
+            <div class="step-find">
+                <div class="title-step">
+                    Faça o seu pedido
+                </div>
+                <div class="icon-step">
+                    <i class="uil uil-list-ul"></i>
+                </div>
+                <div class="text-step">
+                    Fale o que você precisa. É rápido e de graça!
+                </div> 
+            </div>
+            <div class="step-find">
+                <div class="title-step">
+                    Faça o seu pedido
+                </div>
+                <div class="icon-step">
+                    <i class="uil uil-list-ul"></i>
+                </div>
+                <div class="text-step">
+                    Fale o que você precisa. É rápido e de graça!
+                </div> 
+            </div>
+            <div class="step-find">
+                <div class="title-step">
+                    Faça o seu pedido
+                </div>
+                <div class="icon-step">
+                    <i class="uil uil-list-ul"></i>
+                </div>
+                <div class="text-step">
+                    Fale o que você precisa. É rápido e de graça!
+                </div> 
+            </div>
+        </div>
+    </div>
+
+    <div class="menu_services">
+        <div class="item-menu_services">
+            <div class="icon-item">
+                <i class="uil uil-clipboard"></i>
+            </div>
+            <div class="id-item">Marcenaria</div>
+        </div>
+        <div class="item-menu_services">
+            <div class="icon-item">
+                <i class="uil uil-clipboard"></i>
+            </div>
+            <div class="id-item">Marcenaria</div>
+        </div>
+        <div class="item-menu_services">
+            <div class="icon-item">
+                <i class="uil uil-clipboard"></i>
+            </div>
+            <div class="id-item">Marcenaria</div>
+        </div>
+        <div class="item-menu_services">
+            <div class="icon-item">
+                <i class="uil uil-clipboard"></i>
+            </div>
+            <div class="id-item">Marcenaria</div>
+        </div>
+    </div>
+</body>
+</html>
